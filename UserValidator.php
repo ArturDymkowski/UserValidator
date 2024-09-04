57ccd798af4ebaf54e547b598f9f4cd72d303d65
ArturDymkowski <arturdymkowski@wp.pl>
2024-09-04 18:09:20 +0200

    create validateEmail function

--- UserValidator.php
+++ UserValidator.php
@@ -4,7 +4,8 @@ class UserValidator
 {
     public function validateEmail(string $email): bool
     {
-
+        $pattern = '/^[a-z]+@[a-z]+.[a-z]{2}+/';
+        return preg_match($pattern, $email);
     }
 
     public function validatePassword(string $password): bool
