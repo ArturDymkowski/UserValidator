c08e34def17d75f9f9107693670ee5b6260bb641
ArturDymkowski <arturdymkowski@wp.pl>
2024-09-04 22:33:02 +0200

    create validatePassword function

--- UserValidator.php
+++ UserValidator.php
@@ -4,12 +4,13 @@ class UserValidator
 {
     public function validateEmail(string $email): bool
     {
-        $pattern = '/^[a-z]+@[a-z]+.[a-z]{2}+/';
+        $pattern = '/^[a-zA-Z]+@[a-z]+\.[a-z]{2}+/';
         return preg_match($pattern, $email);
     }
 
     public function validatePassword(string $password): bool
     {
-
+        $pattern = '/(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}/';
+        return preg_match($pattern, $password);
     }
 }
--- index.php
+++ index.php
@@ -5,3 +5,15 @@ require_once('UserValidator.php');
 $validator = new UserValidator();
 $email = 'test@example.com';
 $password = "StrongPass1!";
+
+if ($validator->validateEmail($email)) {
+    echo "Email is valid.\n";
+} else {
+    echo "Email is invalid.\n";
+}
+
+if ($validator->validatePassword($password)) {
+    echo "Password is valid.\n";
+} else {
+    echo "Password is invalid.\n";
+}
